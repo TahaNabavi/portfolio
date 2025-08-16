0a3b2ede2c2fded472efed005e360ceb7d91f4e8
Taha Nabavi <wwwdwy643@gmail.com>
2025-08-16 10:43:53 +0330

    feat: add suspense for animation

--- src/providers/transition/index.tsx
+++ src/providers/transition/index.tsx
@@ -1,35 +1,33 @@
 "use client";
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, Suspense } from "react";
 import { useTransitionStore } from "./store";
 import { AnimatePresence, motion } from "framer-motion";
 
 const TransitionOverlay: React.FC = () => {
-  const isTransitioning = useTransitionStore((state) => state.isTransitioning);
   return (
-    <AnimatePresence>
-      {isTransitioning && (
-        <motion.div
-          key="transition"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.25 }}
-          className="fixed inset-0 z-[9999] bg-black backdrop-blur-3xl
+    <motion.div
+      key="transition"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.25 }}
+      className="fixed inset-0 z-[9999] bg-black backdrop-blur-3xl
                      bg-[url('/noise.svg')] bg-repeat 
                      mix-blend-overlay pointer-events-none"
-        />
-      )}
-    </AnimatePresence>
+    />
   );
 };
 
 export const TransitionProvider: React.FC<PropsWithChildren> = ({
   children,
 }) => {
+  const isTransitioning = useTransitionStore((state) => state.isTransitioning);
   return (
     <>
-      <TransitionOverlay />
-      {children}
+      <AnimatePresence>
+        {isTransitioning && <TransitionOverlay />}
+      </AnimatePresence>
+      <Suspense fallback={<TransitionOverlay />}>{children}</Suspense>
     </>
   );
 };
